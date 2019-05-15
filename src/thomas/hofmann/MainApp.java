336ee03217641c2613ff78f0ebb64361bcdcd486
Thomas <thomas.hofmann2003@gmail.com>
2019-05-15 21:11:25 +0200

    Reformatting
    Updated to newest version of NN4J and beautified code

--- src/thomas/hofmann/MainApp.java
+++ src/thomas/hofmann/MainApp.java
@@ -2,44 +2,45 @@ package thomas.hofmann;
 
 import java.util.ArrayList;
 
-public class MainApp
-{
+public class MainApp {
 	public static void main(String[] args) {
-		//Initializing the NeuralNetwork
+		// Initializing the NeuralNetwork
 		NeuralNetwork nn = new NeuralNetwork();
-		
-		//Adding the needed Layers
-		nn.addInputLayer(1,ActFunctions.RELU);
+
+		// Adding the needed Layers
+		nn.addInputLayer(1, ActFunctions.RELU);
 		nn.addHiddenLayer(5, ActFunctions.RELU);
 		nn.addHiddenLayer(2, ActFunctions.TANH);
 		nn.addOutputLayer(1, ActFunctions.TANH);
-		
-		//Initializing the input/output-ArrayList
+
+		// Initializing the input/output-ArrayList
 		ArrayList<Double> input = new ArrayList<>();
 		ArrayList<Double> output = new ArrayList<>();
-		
-		//Adding inputs to the list	('nn.compute' expects all input-nodes to be fed with a value)
+
+		// Adding inputs to the list ('nn.compute' expects all input-nodes to be fed
+		// with a value)
 		input.add(9.1);
-		
-		//Changes the current value of the weight/bias randomly by the mutation-rate
+
+		// Changes the current value of the weight/bias randomly by the mutation-rate
 		nn.mutateWeights(0.1);
 		nn.mutateBias(0.1);
-		
-		//Sets the weight of one specific node 
+
+		// Sets the weight of one specific node
 		nn.setWeight(0, 0, 0, 0.5);
 		nn.setBias(1, 0, 1.0);
-		
-		//Randomizes every weight of the NN
+
+		// Randomizes every weight of the NN
 		nn.randomizeWeights(-1, 1);
-		
-		//Calculating the result for the NeuralNetwork
-		output=nn.compute(input);
-		
-		//Printing the output to the console
+
+		// Calculating the result for the NeuralNetwork
+		output = nn.compute(input);
+
+		// Printing the output to the console
 		System.out.println(output);
-		//Plotting the NeuralNetwork
-		//To use this feature you need to add the AbstractWindwoGraphicsEngine to the build path
+		// Plotting the NeuralNetwork
+		// To use this feature you need to add the AbstractWindwoGraphicsEngine to the
+		// build path
 		Plotter p = new Plotter();
-		p.plot(nn, 50, 125,75,10);
+		p.plot(nn, 50, 125, 75, 10);
 	}
 }
